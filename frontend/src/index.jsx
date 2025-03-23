2656e725625ea696d1d5924714492d8c3a2925c8
hup-creative <hupcreativestudio@gmail.com>
2025-03-23 13:52:04 -0400

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';  // Changed from './App' to './MainApp'
+import App from './App';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
